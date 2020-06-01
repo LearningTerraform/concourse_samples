1cc4f351f7563efff17ff4fa7a7f616d8da08f52
vvijay777 <sftesting7777@gmail.com>
2020-06-01 13:52:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "base_dynamo_table" {
   source = "../../base_module/"    
-  table_name = "aaa-cache"
-  table_attribute = "userID"
+  table_name = "bbb-cache"
+  table_attribute = "Users"
   attribute_type = "S"
   enable_point_in_time_recovery = "false"
   region="ap-south-1"
